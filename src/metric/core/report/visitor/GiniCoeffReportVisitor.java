520c887c61542bce2cbab37917885f09461761b7
jthayes <jthayes@39f5e6c8-f834-0410-9fdf-291f3a001dc0>
2007-08-30 10:26:20 +0000

    Gini report no longer uses a max. value.

--- src/metric/core/report/visitor/GiniCoeffReportVisitor.java
+++ src/metric/core/report/visitor/GiniCoeffReportVisitor.java
@@ -8,6 +8,7 @@ import java.util.Map.Entry;
 
 import metric.core.ReportDefinition;
 import metric.core.exception.ReportException;
+import metric.core.model.ClassMetricData;
 import metric.core.model.HistoryMetricData;
 import metric.core.model.VersionMetricData;
 import metric.core.report.Report;
@@ -28,7 +29,6 @@ import cern.jet.stat.Descriptive;
  */
 public class GiniCoeffReportVisitor extends Report
 {
-	private int maxValue;
 	private String[] fields;
 
 	public GiniCoeffReportVisitor(ReportDefinition m) throws ReportException
@@ -41,13 +41,12 @@ public class GiniCoeffReportVisitor extends Report
 	{
 		try
 		{
-			maxValue = (Integer) rd.args[0];
 			try
 			{
-				fields = StringUtils.asStrings((Object[]) rd.args[1]);
+				fields = StringUtils.asStrings((Object[]) rd.args[0]);
 			} catch (ClassCastException e)
 			{
-				fields = new String[] { (String) rd.args[1] };
+				fields = new String[] { (String) rd.args[0] };
 			}
 		} catch (ArrayIndexOutOfBoundsException e)
 		{
@@ -61,7 +60,6 @@ public class GiniCoeffReportVisitor extends Report
 		HashMap<String, Object> h = new HashMap<String, Object>();
 		String[] constrainedValues = ClassMetric.toStrings();
 		h.put("Metric", StringUtils.sortAndExpand(fields, constrainedValues));
-		h.put("MaxValue", maxValue);
 		return h;
 	}
 
@@ -74,9 +72,7 @@ public class GiniCoeffReportVisitor extends Report
 		while (it.hasNext())
 		{
 			Entry<String, Object> e = (Entry<String, Object>) it.next();
-			if (e.getKey().equals("Max_Value"))
-				maxValue = (Integer) e.getValue();
-			else if (e.getKey().equals("Metric"))
+			if (e.getKey().equals("Metric"))
 			{
 				ArrayList<String> list = (ArrayList<String>) e.getValue();
 				fields = StringUtils.asStrings(list);
@@ -94,7 +90,7 @@ public class GiniCoeffReportVisitor extends Report
 		{
 			VersionMetricData vmd = hmd.getVersion(i);
 			updateProgress(i, total, vmd);
-			rows.add(getGiniCoeffRow(vmd, maxValue, fields));
+			rows.add(getGiniCoeffRow(vmd, fields));
 		}
 
 		// Create and set table.
@@ -108,7 +104,7 @@ public class GiniCoeffReportVisitor extends Report
 	@Override
 	public void visit(VersionMetricData vmd) throws ReportException
 	{
-		String[] row = getGiniCoeffRow(vmd, maxValue, fields);
+		String[] row = getGiniCoeffRow(vmd, fields);
 
 		// Create and set table.
 		MetricTable<String, String> et = new MetricTable<String, String>(getHeading(fields), rd.description);
@@ -135,7 +131,7 @@ public class GiniCoeffReportVisitor extends Report
 		return StringUtils.asStrings(heading);
 	}
 
-	public String[] getGiniCoeffRow(VersionMetricData vmd, int maxValue, String[] fields)
+	public String[] getGiniCoeffRow(VersionMetricData vmd, String[] fields)
 	{
 		ArrayList<String> tmpRow = new ArrayList<String>();
 		tmpRow.add(vmd.get(Version.NAME));
@@ -143,31 +139,124 @@ public class GiniCoeffReportVisitor extends Report
 		tmpRow.add(vmd.get(Version.ID));
 
 		for (String metric : fields)
-			tmpRow.add(StatUtils.toFixedDecPlaces(getGiniCoeff(vmd, metric, maxValue), 3) + "");
+			tmpRow.add(StatUtils.toFixedDecPlaces(getGiniCoeff(vmd, metric), 4) + "");
 		String[] row = new String[tmpRow.size()];
 		tmpRow.toArray(row);
 		return row;
 	}
 
-	private double getGiniCoeff(VersionMetricData vmd, String field, int maxValue)
+	private double getGiniCoeff(VersionMetricData vmd, String field)
 	{
-		int[] data = vmd.getMetricRange(ClassMetric.parse(field));
-		data = StatUtils.createFreqTable(data, maxValue);
-
 		DoubleArrayList d = new DoubleArrayList();
-		for (int i : data)
-			d.add(i);
+		for (ClassMetricData cm : vmd.metricData.values())
+		{
+			if ((field.equals(ClassMetric.LOAD_COUNT.toString()))
+					&& (cm.getSimpleMetric(ClassMetric.IS_INTERFACE) == 1))
+				continue;
+			if ((field.equals(ClassMetric.STORE_COUNT.toString()))
+					&& (cm.getSimpleMetric(ClassMetric.IS_INTERFACE) == 1))
+				continue;
+			if ((field.equals(ClassMetric.BRANCH_COUNT.toString()))
+					&& (cm.getSimpleMetric(ClassMetric.IS_INTERFACE) == 1))
+				continue;
+			if ((field.equals(ClassMetric.FIELD_COUNT.toString()))
+					&& (cm.getSimpleMetric(ClassMetric.IS_INTERFACE) == 1))
+				continue;
+			if ((field.equals(ClassMetric.METHOD_CALL_COUNT.toString()))
+					&& (cm.getSimpleMetric(ClassMetric.IS_INTERFACE) == 1))
+				continue;
+			if ((field.equals(ClassMetric.TYPE_CONSTRUCTION_COUNT.toString()))
+					&& (cm.getSimpleMetric(ClassMetric.IS_INTERFACE) == 1))
+				continue;
+			d.add(cm.getSimpleMetric(ClassMetric.parse(field)));
+		}
+
 		double relVars = 0;
+		double descMean = Descriptive.mean(d);
+
 		for (int i = 0; i < d.size(); i++)
 		{
 			for (int j = 0; j < d.size(); j++)
 			{
 				if (i == j)
 					continue;
-				relVars += (Math.abs(d.get(i) - d.get(j))) / Descriptive.mean(d);
+				relVars += (Math.abs(d.get(i) - d.get(j))); // / descMean;
 			}
 		}
-		return (relVars / (2 * Math.pow(d.size(), 2)));
+		relVars = relVars / (2 * d.size() * d.size());
+		double gini = relVars / descMean;
+
+		return gini;
+
 	}
+
+	// public void printGiniCoeff(String[] fields) throws Exception
+	// {
+	// if (fields.length == 0)
+	// return;
+	// for (int i = 1; i <= versions.size(); i++)
+	// {
+	// Version v = versions.get(i);
+	// double[] giniValues = v.getGiniValues(fields);
+	// double distance = 0.0;
+	// if (i > 1)
+	// distance = Version.calcDistance(giniValues, versions.get(i -
+    // 1).getGiniValues(fields));
+	// System.out.printf("%10s\t%2d\t%10s\t%5d\t", shortName, v.RSN, v.id,
+    // v.getClassCount());
+	// for (int j = 0; j < giniValues.length; j++)
+	// {
+	// System.out.printf("%5.4f\t", giniValues[j]);
+	// }
+	// System.out.printf("%5.4f\n", distance);
+	// }
+	// }
+	//
+	// public double[] getGiniValues(String[] fields) throws Exception
+	// {
+	// double[] giniValues = new double[fields.length];
+	// for (int j = 0; j < fields.length; j++)
+	// giniValues[j] = calcGiniCoefficient(fields[j]);
+	// return giniValues;
+	// }
+	//
+	// public double calcGiniCoefficient(String field) throws Exception
+	// {
+	// DoubleArrayList d = new DoubleArrayList();
+	// for (ClassMetric cm : metricData.values())
+	// {
+	// if ((field.equals("loadCount")) && (cm.isInterface == 1))
+	// continue;
+	// if ((field.equals("storeCount")) && (cm.isInterface == 1))
+	// continue;
+	// if ((field.equals("branchCount")) && (cm.isInterface == 1))
+	// continue;
+	// if ((field.equals("fieldCount")) && (cm.isInterface == 1))
+	// continue;
+	// if ((field.equals("methodCallCount")) && (cm.isInterface == 1))
+	// continue;
+	// if ((field.equals("typeConstructionCount")) && (cm.isInterface == 1))
+	// continue;
+	// d.add(cm.getMetricValue(field));
+	// }
+	//
+	// double relVars = 0;
+	// double descMean = Descriptive.mean(d);
+	//
+	// for (int i = 0; i < d.size(); i++)
+	// {
+	// for (int j = 0; j < d.size(); j++)
+	// {
+	// if (i == j)
+	// continue;
+	// relVars += (Math.abs(d.get(i) - d.get(j))); // / descMean;
+	// }
+	// }
+	// relVars = relVars / (2 * d.size() * d.size());
+	// double gini = relVars / descMean;
+	//
+	// // System.out.println("Gini: " + gini);
+	// return gini;
+	// }
 
 }
